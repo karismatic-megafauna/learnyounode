3f8e0ad91672544a2f30fe78cfdccd6829d2745a
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-08 09:56:54 -0800

    It werrrrks! parsing multiple requests and respond
    to them based on different pathnames
    weee! that was fun. I know node a little better meow.

--- json-api-server.js
+++ json-api-server.js
@@ -1,12 +1,33 @@
-'use strict'
-let http = require('http');
-let url = require('url');
+var http = require('http');
+var url = require('url');
 
-let server = http.createServer(function (req, res) {
+function getTime(time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function getUnixTime(time) {
+  return {
+    unixtime: time.getTime()
+  }
+}
+
+var server = http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, 'true');
-  console.log(parsedUrl);
-  res.writeHead(200, { 'Content-Type': 'application/json'});
+  var time = new Date(parsedUrl.query.iso);
+  var data;
 
+  if("/api/parsetime" == parsedUrl.pathname){
+    data = getTime(time);
+  } else if ("/api/unixtime" == parsedUrl.pathname){
+    data = getUnixTime(time);
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json'});
+  res.end(JSON.stringify(data));
 });
 
 server.listen(Number(process.argv[2]));
