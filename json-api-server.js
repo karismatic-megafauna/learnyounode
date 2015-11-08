31a2c9e95daef21911a3b9e46cfa259222a85990
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-08 08:56:38 -0800

    Parsing the URL

--- json-api-server.js
+++ json-api-server.js
@@ -3,7 +3,10 @@ let http = require('http');
 let url = require('url');
 
 let server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url, 'true');
+  console.log(parsedUrl);
   res.writeHead(200, { 'Content-Type': 'application/json'});
+
 });
 
 server.listen(Number(process.argv[2]));
